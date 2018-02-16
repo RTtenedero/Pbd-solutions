f1709af313443a6ab6a926df6b1a93f7c12cd5b7
RTtenedero <36413776+RTtenedero@users.noreply.github.com>
2018-02-16 10:56:32 -0500

    Add files via upload

---
+++ 04/WhatToDo.java
@@ -0,0 +1,16 @@
+The two slashes at the beginning of the print ln statements causes an error when the computer runs the java file.
+public class What To DO on your own
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello Again");
+		System.out.println("I like typing this."); 
+		System.out.println("This is fun.");
+		System.out.println("Yay! Printing.");
+		System.out.println("I'd much rather you 'not'sir.");
+		System.out.println("I \"said\" do not touch this.");
+		System.out.println//("Yaay, I did it");
+	}
+}
+
