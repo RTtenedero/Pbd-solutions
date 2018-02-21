29cff02141c5db2dc1ac81b4a21cf762ea6803fa
RTtenedero <36413776+RTtenedero@users.noreply.github.com>
2018-02-21 11:08:32 -0500

    Add files via upload

---
+++ 02/MoreVariablesandPrinting.java
@@ -0,0 +1,19 @@
+public class MoreVariablesAndPrinting {
+    public static void main(String[] args) {
+        String myName, myEyes, myTeeth, myHair;
+        int myAge, myHeight, myWeight;
+        
+        myName = "Greg Dofs";
+        myAge = 53;
+        myHeight = 86;
+        myWeight = 180;
+        myEyes = "Green";
+        myTeeth = "White";
+        myHair = "Brown";
+        
+        System.out.println("Let's talk about" + myName + ".");
+        System.out.println("He's" + myHeight + "inches tall.");
+        System.out.println("He's" + myHeight + "pounds heavy.");
+        System.out.println("Actually, that's not too heavy.");
+        System.out.println(
+        System.out.println
