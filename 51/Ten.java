77018b91a9c88bb3f97d55e6e7fbbe202a3436ad
RTtenedero <36413776+RTtenedero@users.noreply.github.com>
2018-03-23 11:09:15 -0400

    Add files via upload

---
+++ 51/Ten.java
@@ -0,0 +1,9 @@
+public class Ten {
+    public static void main(String[] args) {
+       
+      for(int w = 1 ; w <= 10 ; w = w + 1) {
+        System.out.println("Mr. Gallo is cool") ;
+        }
+        
+   }
+ }
